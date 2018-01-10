7616146fed6efd598e1eb57e506af1eae271b175
Matthew <mrknowles55@gmail.com>
2018-01-10 02:30:40 -0500

    First Commit

---
+++ ezsprite.py
@@ -0,0 +1,20 @@
+"""
+
+Collection of functions and classes to ease with the use of sprites in PyGame programs.
+
+Example:
+    pass
+
+Attributes:
+
+
+TODO:
+    * function to upload sprite to memory
+    * function to upload multiple sprites
+    * class to store basic sprite functions
+    * function to handle image movement for sprites that change over time
+    * Add to Github
+    * Function to edit sprites (such as making background transparent or resizing)
+    * Add Examples
+    * Add Attributes
+"""
