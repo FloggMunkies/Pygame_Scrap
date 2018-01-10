97d8360802fea58c66635e04c19a3665da00b01e
Matthew <mrknowles55@gmail.com>
2018-01-10 03:58:45 -0500

    Added basic Pygame program and setup basic Docstring.

--- First.py
+++ First.py
@@ -1,1 +1,75 @@
-import pygame
+# Imports
+import pygame as pg
+from pygame.locals import *
+import os, sys
+
+# Functions
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('images', name)
+    try:
+        image = pg.image.load(fullname)
+    except pg.error:
+        print("Cannot load image:", name)
+        raise SystemExit
+
+    image = image.convert()
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey, RLEACCEL)
+    return image, image.get_rect()
+
+
+
+# Classes
+
+
+class App:
+    def __init__(self):
+        self._running = True
+        self._display_surf = None
+        self.size = self.weight, self.height = 640, 400
+
+    def on_init(self):
+        pg.init()
+        self._display_surf = pg.display.set_mode(self.size, pg.HWSURFACE | pg.DOUBLEBUF)
+        self._running = True
+
+    def on_event(self, event):
+        if event.type == pg.QUIT:
+            self._running = False
+
+    def on_loop(self):
+        pass
+
+    def on_render(self):
+        pass
+
+    def on_cleanup(self):
+        pg.quit()
+
+    def on_execute(self):
+        if self.on_init() == False:
+            self._running = False
+
+        while (self._running):
+            for event in pg.event.get():
+                self.on_event(event)
+            self.on_loop()
+            self.on_render()
+        self.on_cleanup()
+
+
+class Foo(pg.sprite.Sprite):
+
+    def __init__(self):
+        pg.sprite.Sprite.__init__(self)  # call Sprite Initializer
+
+# Main Loop
+
+
+if __name__ == "__main__":
+    theApp = App()
+    theApp.on_execute()
--- ezsprite.py
+++ ezsprite.py
@@ -1,3 +1,5 @@
+import pygame as pg
+import os
 """
 
 Collection of functions and classes to ease with the use of sprites in PyGame programs.
@@ -18,3 +20,45 @@ TODO:
     * Add Examples
     * Add Attributes
 """
+
+# functions
+
+
+def load_image_basic(path, color_key=None):
+    """Loads a sprite in the most basic fashion with a color key included.
+
+    Takes path to an image and loads it as an pygame image object and gets its rect dimensions.
+    Uses the color key to make a certain color of the image transparent, useful for sprites but not for textures.
+    Color key is optional or takes an RGB tuple, such as (255, 0, 0) for red, or -1 to grab the color in the upper-left
+    pixel of the image and uses that.
+
+    :param path: String
+                    Path to image
+    :param color_key: Tuple or -1, optional(None)
+                    The color that's to be transparent, RGB tuple or -1 which uses the pixel in the upper-left corner
+    :return: Tuple
+                    Image object and image.rect()
+    :raises:
+    SystemExit:
+                    If image fails to load
+
+    """
+    try:
+        image = pg.image.load(path)
+    except pg.error:
+        print("Cannot load image:", path)
+        raise SystemExit
+    image = image.convert()
+    if color_key is not None:
+        if color_key is -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key, pg.locals.RLEACCEL)
+    return image, image.get_rect()
+
+# classes
+
+
+class SpriteGopher:
+    def __init__(self):
+        # TODO implement this class
+        pass
